df2cb8e42a72e6f3b6f3dfb434a178430c5a4b3d
Jimmy <jyng02@gmail.com>
2017-12-12 13:50:48 -0800

    build system

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require ('gulp');
+var sass = require ('gulp-sass');
+var webserver = require('gulp-webserver');
+gulp.task('styles',function(){
+  return gulp.src('./sass/**/*.scss')
+  .pipe(sass().on('error',sass.logError))
+  .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./sass/**/*.scss',['styles']);
+});
+
+gulp.task('webserver',function(){
+  gulp.src('.')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+})
+gulp.task('serve',['webserver','watch']);
